9b88fffea63d5a360f3cd29074f09136c0d17320
PavankumarVivekanandhan <112475657+PavankumarVivekanandhan@users.noreply.github.com>
2022-09-01 02:08:50 -0700

    day 6

---
+++ pavan.py
@@ -0,0 +1,35 @@
+Python 3.10.6 (tags/v3.10.6:9c7b4bd, Aug  1 2022, 21:53:49) [MSC v.1932 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+t1=(1,2,3)
+t2=(2,1,3)
+print(t1+t2)
+(1, 2, 3, 2, 1, 3)
+primt(t1*2)
+Traceback (most recent call last):
+  File "<pyshell#3>", line 1, in <module>
+    primt(t1*2)
+NameError: name 'primt' is not defined. Did you mean: 'print'?
+print(t1*2)
+(1, 2, 3, 1, 2, 3)
+print(2int1)_
+SyntaxError: invalid decimal literal
+print(5 in t1)
+False
+print(min(T1))
+Traceback (most recent call last):
+  File "<pyshell#7>", line 1, in <module>
+    print(min(T1))
+NameError: name 'T1' is not defined. Did you mean: 't1'?
+print(min(t1))
+1
+print(max(t2))
+3
+print(len(t2))
+3
+t3=(t1,t2)
+print(t3)
+((1, 2, 3), (2, 1, 3))
+print(t1[1:])
+(2, 3)
+print(t1[::-1])
+(3, 2, 1)
